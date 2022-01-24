13068e0d1b838654d91d45b5de6dcfa7b58ac844
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2022-01-24 10:50:11 -0600

    ✨ Certification card

---
+++ src/app/containers/components/card-certification/card-certification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-certification',
+  templateUrl: './card-certification.component.html',
+  styleUrls: ['./card-certification.component.sass']
+})
+export class CardCertificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
