5aa009fed0d261bcbb5f0dd831a970f9efd5096e
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2022-02-06 21:40:47 -0600

    ✨Home typewriting

--- src/app/containers/pages/home/home.component.ts
+++ src/app/containers/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,21 +8,45 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit {
 
+  @ViewChild('textP') refTextP!: ElementRef<HTMLParagraphElement>;
+
   loading = true;
   themeDark: any;
+  about = "Ingeniero en sistemas computacionales, especializado en construir y ocasionalmente diseñar excepcionales experiencias digitales. Buscando crear la mejor versión de mí, amante de aprender, compartir conocimiento y dejar huella.";
 
   constructor() { }
 
   ngOnInit(): void {
     const currentHour = new Date().getHours();
     this.themeDark = sessionStorage.getItem('themeDark') === null
-    ? !(currentHour >= 7 && currentHour <= 19)
-    : sessionStorage.getItem('themeDark') === 'true';
+      ? !(currentHour >= 7 && currentHour <= 19)
+      : sessionStorage.getItem('themeDark') === 'true';
     this.themeDark
-    ? document.body.classList.add('dark')
-    : document.body.classList.remove('dark');
-    setTimeout(() => this.loading = false, 2500);
+      ? document.body.classList.add('dark')
+      : document.body.classList.remove('dark');
     document.getElementById('colorBg')?.setAttribute('data-dark', `${this.themeDark}`)
+    setTimeout(() => {
+      this.loading = false
+      this.typeText()
+    }, 2500);
+  }
+
+
+  typeText() {
+    let i = 0;
+    let sentence = "";
+    const putLetter = () => {
+      if (i === this.about.length) return;
+      sentence += this.about[i];
+      setTimeout(() =>
+        this.refTextP.nativeElement.innerText = sentence + " |"
+        , 0);
+      // sentence += this.about[i] === '.' && this.about.length - 1 !== i ? '\n' : '';
+      if (this.about[i] === '.') setTimeout(putLetter, 600);
+      else setTimeout(putLetter, 30);
+      i++;
+    }
+    putLetter();
   }
 
 }
