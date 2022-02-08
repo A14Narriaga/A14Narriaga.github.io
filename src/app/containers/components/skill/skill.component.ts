38bf80e16eb1efd33b979eb5a92e99a3eb7138f4
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2022-02-08 09:05:21 -0600

    ✨ Skills

--- src/app/containers/components/skill/skill.component.ts
+++ src/app/containers/components/skill/skill.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.scss']
 })
-export class SkillComponent implements OnInit {
 
-  constructor() { }
+export class SkillComponent {
 
-  ngOnInit(): void {
-  }
+  @Input() name: string = "name";
+  @Input() level: number = 10;
+  @Input() icon: string = "fas fa-smile-beam";
 
 }
