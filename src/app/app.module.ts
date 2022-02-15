1ac534cd050544f08db15388183353384462ea4d
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2022-02-15 10:06:19 -0600

    🐎 Icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,24 @@ import { CodeComponent } from './containers/components/code/code.component';
 import { NavbarComponent } from './containers/components/navbar/navbar.component';
 import { ProfilePictureComponent } from './containers/components/profile-picture/profile-picture.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { IconAngularComponent } from './containers/icons/IconAngular.component';
+import { IconReactComponent } from './containers/icons/IconReact.component';
+import { IconJavaScriptComponent } from './containers/icons/IconJavaScript.component';
+import { IconTypeScriptComponent } from './containers/icons/IconTypeScript.component';
+import { IconHTML5Component } from './containers/icons/IconHTML5.component';
+import { IconPugComponent } from './containers/icons/IconPug.component';
+import { IconCSS3Component } from './containers/icons/IconCSS3.component';
+import { IconSASSComponent } from './containers/icons/IconSASS.component';
+import { IconNodeJSComponent } from './containers/icons/IconNodeJS.component';
+import { IconPHPComponent } from './containers/icons/IconPHP.component';
+import { IconMySQLComponent } from './containers/icons/IconMySQL.component';
+import { IconRedisComponent } from './containers/icons/IconRedis.component';
+import { IconFigmaComponent } from './containers/icons/IconFigma.component';
+import { IconAdobeXDComponent } from './containers/icons/IconAdobeXD.component';
+import { IconVSCodeComponent } from './containers/icons/IconVSCode.component';
+import { IconGitComponent } from './containers/icons/IconGit.component';
+import { IconVimComponent } from './containers/icons/IconVim.component';
+import { IconLaTeXComponent } from './containers/icons/IconLatex.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +46,25 @@ import { ReactiveFormsModule } from '@angular/forms';
     CardBlogComponent,
     CodeComponent,
     NavbarComponent,
-    ProfilePictureComponent
+    ProfilePictureComponent,
+    IconAngularComponent,
+    IconReactComponent,
+    IconJavaScriptComponent,
+    IconTypeScriptComponent,
+    IconHTML5Component,
+    IconPugComponent,
+    IconCSS3Component,
+    IconSASSComponent,
+    IconNodeJSComponent,
+    IconPHPComponent,
+    IconMySQLComponent,
+    IconRedisComponent,
+    IconFigmaComponent,
+    IconAdobeXDComponent,
+    IconVSCodeComponent,
+    IconGitComponent,
+    IconVimComponent,
+    IconLaTeXComponent
   ],
   imports: [
     BrowserModule,
